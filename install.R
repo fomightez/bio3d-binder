a34abcc5b5d127a12ce87128d758484feafe9311
Wayne's Bioinformatics Code Portal <fomightez@gmail.com>
2018-10-17 12:53:39 -0400

    remove blanks

--- install.R
+++ install.R
@@ -6,5 +6,3 @@ install.packages("devtools")
 install.packages("XML")
 library(devtools)
 install_bitbucket("Grantlab/bio3d", subdir="ver_devel/bio3d/") #needed for XML, see https://bitbucket.org/Grantlab/bio3d/issues/545/error-xml-content-does-not-seem-to-be-xml
-
-
